077ffa78454e21c67c0a459802735e799c63d7d2
pipi-shortstocking <jannie435@naver.com>
2022-11-17 16:31:21 +0900

    ui ver.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Table from './component/Table/Table'
+
+const list = [
+  {이름: "가가가", 성별: "남", 주민번호: "111111-2222222", 발생일: "2016-02-13", 발생시분: "11:23", 발생장소: "서울시 노원구"},
+  {이름: "나나나", 성별: "여", 주민번호: "222222-1111111", 발생일: "2014-10-23", 발생시분: "21:10", 발생장소: "서울시 서대문구"},
+  {이름: "다다다", 성별: "남", 주민번호: "111111-2222222", 발생일: "2020-08-05", 발생시분: "08:40", 발생장소: "대전시 서구"},
+  {이름: "라라라", 성별: "여", 주민번호: "222222-1111111", 발생일: "2018-04-10", 발생시분: "20:22", 발생장소: "부산시 해운대구"},
+]
+
+const colNames = ['이름', '성별', '주민번호', '발생일', '발생시분', '발생장소'] /* 항목을 추가해주면 됨 */
+
+class App extends Component {
+  render() {
+    return <div className="App">
+      <Table list={list} colNames={colNames}/>
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/component/Table/Table.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Table({ list, colNames, width = "auto", height = "auto"}) {
+    return (
+        <div style ={{ width: '100%',  boxShadow: '3px 6px 3px #ccc'}}> {/* width의 비율로 화면에 표시될 모든 가로폭 설정 */}
+            <h2> 실종 아동 신고 어드민 페이지 </h2> {/* 페이지 상단 제목 */}
+            <p>신고 접수 현황이 확인 가능합니다</p> {/* 페이지 부가 설명 */}
+
+            {list.length > 0 && (
+                <table 
+                cellSpacing ="0"
+                style={{width: '100%', height: height, padding: 'Spx 10px' }}
+                >
+                    <thead style={{backgroundColor: 'black', color: 'white' }}> {/* 색감 조정 */}
+                        <tr>
+                            {colNames.map((headerItem, index) => (
+                                <th key={index}>{headerItem.toUpperCase()}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.values(list).map((obj, index) => (
+                            <tr key={index}>
+                                {Object.values(obj).map((value, index2) => (
+                                    <td key={index2}>{value}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    )
+}
+
+export default Table
