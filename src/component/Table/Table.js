a6ebe331b25e2ba829d745904f3bfb862a35754e
pipi-shortstocking <jannie435@naver.com>
2022-11-17 16:57:51 +0900

    add img

--- src/component/Table/Table.js
+++ src/component/Table/Table.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Table({ list, colNames, width = "auto", height = "auto"}) {
     return (
         <div style ={{ width: '100%',  boxShadow: '3px 6px 3px #ccc'}}> {/* width의 비율로 화면에 표시될 모든 가로폭 설정 */}
-            <h2> 실종 아동 신고 어드민 페이지 </h2> {/* 페이지 상단 제목 */}
+            <h2> 실종 아동 신고 어드민 페이지 <img src="https://user-images.githubusercontent.com/95032287/202388454-3f650c2e-0c64-40ef-badf-76910c0a8fd1.png" width = "10%" height = "10%"/></h2> {/* 페이지 상단 제목 */}
             <p>신고 접수 현황이 확인 가능합니다</p> {/* 페이지 부가 설명 */}
 
             {list.length > 0 && (
